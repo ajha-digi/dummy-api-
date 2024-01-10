0434530b385f3f580fca63b9f04a193968897ad2
ajha-digi <ajha@digisprint.com>
2024-01-10 17:48:42 +0530

    Added curd operation in express app

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const cors = require("cors");
- const PORT = process.env.PORT || 7000
+require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT;
+
 app.use(cors());
-dotenv.config({});
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.json({
-        message: "Hello from Express app !!"
+const users = [
+  {
+    id: 1,
+    name: "test 1",
+  },
+  {
+    id: 2,
+    name: "test 2",
+  },
+  {
+    id: 3,
+    name: "test 3",
+  },
+];
+
+app.get("/", function (req, res) {
+  res.json(users);
+});
+
+app.get("/:id", function (req, res) {
+  const { id } = req.params;
+  res.json(
+    users.filter(function (user) {
+      return user.id === +id;
     })
-})
+  );
+});
 
-app.listen(PORT, ()=>{
-    console.log(`App running on http://localhost:${PORT}`)
-})
+app.post("/", function (req, res) {
+  users.push({ id: users.length + 1, ...req.body });
+  res.json({ code: "200", message: "success", data: users });
+});
+
+app.put("/:id", function (req, res) {
+  const { id } = req.params;
+  const { name } = req.body;
+  const index = users.findIndex(function(user){
+    return user.id === +id
+  })
+  if(index > -1) {
+    users[index] = {
+        ...users[index],
+        name
+    }
+  }
+  res.json(users);
+});
+
+app.delete("/:id", function (req, res) {
+    const { id } = req.params;
+    const index = users.findIndex(function(user){
+        return user.id === +id
+      })
+    users.splice(index,1)
+  res.json(users);
+});
+
+app.listen(PORT, function () {
+  console.log(`express server is running at http://localhost:${PORT}`);
+});
